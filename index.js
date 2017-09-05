1028c56c650850ce645f429f7c27e502f8d671e9
Patrick Brouwer <patrick@inlet.nl>
2017-09-05 14:32:03 +0200

    Cleanup

--- index.js
+++ index.js
@@ -1,32 +1,27 @@
 #!/usr/bin/env node
 
-const fs = require('fs')
-const path = require('path')
-const readline = require('readline')
-const google = require('googleapis')
-const GoogleAuth = require('google-auth-library')
-const program = require('commander')
+const fs         = require('fs'),
+      path       = require('path'),
+      readline   = require('readline'),
+      google     = require('googleapis'),
+      GoogleAuth = require('google-auth-library'),
+      program    = require('commander')
 
-const SECRET_PATH = path.resolve(__dirname, 'client_secret.json')
-const TOKEN_PATH = path.resolve(__dirname, 'token.json')
-const SCOPES = ['https://www.googleapis.com/auth/gmail.settings.basic']
+const SECRET_PATH = path.resolve(__dirname, 'client_secret.json'),
+      TOKEN_PATH  = path.resolve(__dirname, 'token.json'),
+      SCOPES      = ['https://www.googleapis.com/auth/gmail.settings.basic']
 
 const filters = google.gmail('v1').users.settings.filters
 
-const question = (interface, msg) =>
-  new Promise(resolve => {
-    interface.question(msg, response => resolve(response))
-  })
+const question = (interface, msg) => new Promise(resolve => {
+  interface.question(msg, response => resolve(response))
+})
 
 async function authorize(credentials) {
-  let {
-    client_secret: clientSecret,
-    client_id: clientId,
-    redirect_uris: [redirectUrl]
-  } = credentials.installed
+  let { client_secret: clientSecret, client_id: clientId, redirect_uris: [redirectUrl] } = credentials.installed
 
-  let auth = new GoogleAuth()
-  let oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl)
+  const auth = new GoogleAuth()
+  const oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl)
 
   let token
 
@@ -42,9 +37,7 @@ async function authorize(credentials) {
 }
 
 async function getNewToken(oauth2Client) {
-  let token, code, authUrl
-
-  authUrl = oauth2Client.generateAuthUrl({
+  const authUrl = oauth2Client.generateAuthUrl({
     access_scope: 'offline',
     scope: SCOPES
   })
@@ -55,24 +48,23 @@ async function getNewToken(oauth2Client) {
     input: process.stdin,
     output: process.stdout
   })
-  code = await question(rl, 'Enter the code from that page here: ')
+
+  const code = await question(rl, 'Enter the code from that page here: ')
   rl.close()
 
   return await getTokenFromOauth2Client(oauth2Client, code)
 }
 
-async function getTokenFromOauth2Client(oauth2Client, code) {
-  return new Promise((resolve, reject) => {
-    oauth2Client.getToken(code, function(err, token) {
-      if (err) {
-        console.log(`Error while trying to retrieve access token`)
-        reject(err)
-        return
-      }
-      resolve(token)
-    })
+const getTokenFromOauth2Client = (oauth2Client, code) => new Promise((resolve, reject) => {
+  oauth2Client.getToken(code, function(err, token) {
+    if (err) {
+      console.log(`Error while trying to retrieve access token`)
+      reject(err)
+      return
+    }
+    resolve(token)
   })
-}
+})
 
 function storeToken(token) {
   fs.writeFileSync(TOKEN_PATH, JSON.stringify(token))
@@ -81,31 +73,26 @@ function storeToken(token) {
 
 /// API METHODS
 
-async function getFilters(oauth2Client) {
-  return new Promise((resolve, reject) => {
-    filters.list({ auth: oauth2Client, userId: 'me' }, function(err, response) {
-      if (err) {
-        return reject(err)
-      }
-      resolve(response)
-    })
+const getFilters = oauth2Client => new Promise((resolve, reject) => {
+  filters.list({ auth: oauth2Client, userId: 'me' }, function(err, response) {
+    if (err) {
+      return reject(err)
+    }
+    resolve(response)
   })
-}
+})
 
-async function createFilter(oauth2Client, criteria, action) {
+const createFilter = (oauth2Client, criteria, action) => new Promise((resolve, reject) => {
   const options = { criteria, action }
   console.log(`Creating filter...`, options)
-
-  return new Promise((resolve, reject) => {
-    filters.create({ auth: oauth2Client, userId: 'me', resource: {...options} }, {}, function(err, response) {
-        if (err) {
-          return reject(err)
-        }
-        resolve(response)
-      }
-    )
+  
+  filters.create({ auth: oauth2Client, userId: 'me', resource: { ...options } }, {}, (err, response) => {
+    if (err) {
+      return reject(err)
+    }
+    resolve(response)
   })
-}
+})
 
 // start
 async function start() {
@@ -114,12 +101,14 @@ async function start() {
     .version('1.0.0')
     .description('Manage Gmail Filters')
     .option('-l --list', 'List all filters')
-    .option('-c, --criteria [from|to|subject|query|negatedQuery|hasAttachment|excludeChats|size|sizeComparison]', 'Set criteria')
+    .option('-c, --criteria [from|to|subject|query|negatedQuery|hasAttachment|excludeChats|size|sizeComparison]',
+      'Set criteria')
     .option('-a --action [addLabelIds|removeLabelIds|forward]', 'Define action')
 
   program.on('--help', function() {
     console.log('')
-    console.log('  Resource reference: https://developers.google.com/gmail/api/v1/reference/users/settings/filters#resource')
+    console.log(
+      '  Resource reference: https://developers.google.com/gmail/api/v1/reference/users/settings/filters#resource')
     console.log('')
     console.log('  Examples:')
     console.log('')
@@ -137,7 +126,7 @@ async function start() {
 
   // authorize app
   const oauth2Client = await authorize(clientSecret)
-  
+
   if (program.list) {
     const filters = await getFilters(oauth2Client)
     console.log(JSON.stringify(filters, null, 2))
